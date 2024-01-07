270e397463a3e87833b8af136827df9a91bb86b1
Thiago Gonçalves <tsyhwh55@gmail.com>
2024-01-07 15:01:48 -0300

    Dynamic attributes for components

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './big-card.component.css'
 })
 export class BigCardComponent {
+  @Input()
+  photoCover: string = "";
+  @Input()
+  title: string = "";
+  @Input()
+  description: string = "";
 
 }
